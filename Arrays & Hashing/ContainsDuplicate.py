ff95a05a63c6d5726c8ed675933eae1f90f9e103
Erwin-R <rerwin684@gmail.com>
2023-10-09 00:43:02 -0500

    added notes

--- Arrays & Hashing/ContainsDuplicate.py
+++ Arrays & Hashing/ContainsDuplicate.py
@@ -1,12 +1,14 @@
 #Problem: Return True if there are duplicates in nums array and False if not
 
 #Solution 1
-#Time: O(n)
-#Space: O(n)
+#Time: O(n) *Iterating over array
+#Space: O(n) *Set will be size of array in worst case
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        #We use set since it can only contain unique values 
         uniqueNums = set()
 
+        #Iterate over array and return False if n is found in set
         for n in nums:
             if n in uniqueNums: 
                 return True
