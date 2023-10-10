a146ffa59bfec440034de532f4dccd397f9eff60
Erwin-R <rerwin684@gmail.com>
2023-10-09 19:27:49 -0500

    added notes

--- Arrays & Hashing/ValidAnagram.py
+++ Arrays & Hashing/ValidAnagram.py
@@ -1,15 +1,17 @@
+#Time: O(n) iterating over strings 
+#Space: O(n) hashmap is the length of strings
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        #Base Case: if length is different then T cannot be anagram of S
         if len(s) != len(t):
             return False
         
+        #create map for both strings to keep track of characters
         sCount, tCount = {}, {}
 
+        #Iterate through characters in strings 
         for i in range(len(s)):
             sCount[s[i]] = 1 + sCount.get(s[i], 0) 
             tCount[t[i]] = 1 + tCount.get(t[i], 0)
-        
-        print(sCount)
-        print(tCount)
 
         return sCount == tCount
