262ec6c3014ec91e887e0310ede67914644b3504
Erwin-R <rerwin684@gmail.com>
2024-03-16 12:36:26 -0500

    added notes

---
+++ Arrays & Hashing/TopKFrequentElements.py
@@ -0,0 +1,15 @@
+#Problem: Given an integer array "nums" and an integer "k", return the k most frequent elements. You may return the answer in any order.
+
+#Constraints:
+    # 1. Length of array nums is at least 1
+    # 2. k is in the range [1, the number of unique elements in the array]
+    # 3. It is guaranteed that the answer is unique
+
+#Test Cases
+    # Example 1:
+        # Input: nums = [1,1,1,2,2,3], k = 2
+        # Output: [1,2]
+    # Example 2:
+        # Input: nums = [1], k = 1
+        # Output: [1]
+
