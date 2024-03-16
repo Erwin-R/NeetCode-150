8ebccbc9f08283ce2dc0d49b3fa5bf9868d7b55e
Erwin-R <rerwin684@gmail.com>
2024-03-16 12:47:40 -0500

    added solution

--- Arrays & Hashing/TopKFrequentElements.py
+++ Arrays & Hashing/TopKFrequentElements.py
@@ -13,3 +13,26 @@
         # Input: nums = [1], k = 1
         # Output: [1]
 
+#Time: O(n), where n is the number of unique elements (~size of input array)
+#Technically O(n1 + n2) where n1 is size of input array and n2 for iterating over array per index 
+#Worst case is where each element has the same number of occurences
+
+#Space: O(n), where N is the size of the hashmap to count occurences of each value
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        #num: # of occurences
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():  #[(num, # of occurences), (n, #Occ)]
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+            if(len(res) == k):
+                return res
