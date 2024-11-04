c1274ba5007cafb546b187d5a56df397efd0c1ad
Erwin-R <rerwin684@gmail.com>
2024-11-04 11:27:13 -0600

    added solution

--- Trees/InvertBinaryTree.py
+++ Trees/InvertBinaryTree.py
@@ -16,3 +16,44 @@ Test Cases:
 
 #Time: O(n) since we are visiting every node in the tree (size of tree)
 #Space: O(h) where h is the height of the tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        #preorder traversal (visit parent, then left children, then right children) and swap the children down until we get to null
+
+        def dfs(root):
+            if not root: 
+                return 
+
+            tmp = root.left
+            root.left = root.right
+            root.right = tmp
+
+            dfs(root.left)
+            dfs(root.right)
+
+            return root
+        
+        return dfs(root)
+    
+
+class Solution:
+    def invertTree2(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        #Swap children 
+
+        tmp = root.left
+        root.left = root.right
+        root.right = tmp
+
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
