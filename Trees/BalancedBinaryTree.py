3625843d38bd6cff1c2cd18ef2754bb582f03167
Erwin-R <rerwin684@gmail.com>
2024-11-06 12:23:30 -0600

    added solution

--- Trees/BalancedBinaryTree.py
+++ Trees/BalancedBinaryTree.py
@@ -16,4 +16,38 @@ Test Cases:
         Output: true
 """
 
+#Time: O(n) where n is amount of nodes in the tree since we are visiting all of them
+#Space: O(n) where n is the height of of the tree
 
+class Solution:
+    def isBalanced1(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root: return [True, 0]
+
+            left, right = dfs(root.left), dfs(root.right)
+            #Balanced is True if...
+            #if the left of right trees are not balanced then we can be sure that the parents are also not balanced
+            balanced = (left[0] and right[0] and abs(left[1] - right[1]) <= 1)
+
+            return [balanced, 1 + max(left[1], right[1])]
+
+        return dfs(root)[0]
+
+class Solution2:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        self.balanced = True
+
+        def dfs(root):
+            if not root: 
+                return 0
+
+            left, right= dfs(root.left), dfs(root.right)
+
+            if abs(left - right) > 1: 
+                self.balanced = False
+
+            return 1 + max(left, right)
+        
+        dfs(root)
+
+        return self.balanced
