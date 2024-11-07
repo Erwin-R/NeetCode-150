1358ed09bc5fe00cdfe223c6d9ccaa947b8c1b26
Erwin-R <rerwin684@gmail.com>
2024-11-07 12:13:04 -0600

    added solution

--- Trees/SubtreeOfAnotherTree.py
+++ Trees/SubtreeOfAnotherTree.py
@@ -14,3 +14,30 @@ Test Cases:
 
 #Time: O(n * m) where n and m are the size of the tree and subroot respectively
 #Space: O(n + m) where n and m are the height of the tree respectively
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        #null subRoot can be a subtree of root
+        #however subRoot cannot be a subtree if root is null
+        if not subRoot: return True
+        if not root: return False
+        
+        #if subTrees are the same then we return true
+        if self.sameTree(root, subRoot):
+            return True
+        
+        #if subTrees are not the same we recursively iterate through one side of 
+        #tree and see if we can reach a value that equals root of subRoot
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+
+    def sameTree(self, root, subRoot):    
+        if not root and not subRoot:
+            return True
+        # if both nodes exist and values are equal we want to recusively iterate
+        # to see if tree is the same
+        if root and subRoot and root.val == subRoot.val: 
+            return (self.sameTree(root.left, subRoot.left) and 
+            self.sameTree(root.right, subRoot.right))
+        
+        #if one node is is non-null and the other is null we return false
+        return False
