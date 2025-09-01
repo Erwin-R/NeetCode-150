de8051d4079dea47bf8f372cfc0445177c3fd8e7
Erwin-R <rerwin684@gmail.com>
2025-09-01 12:36:54 -0500

    added solution to kth largest element in an array

--- Heap & Priority Queue/KClosestPointstoOrigin.py
+++ Heap & Priority Queue/KClosestPointstoOrigin.py
@@ -14,10 +14,29 @@ Test Cases:
         The distance between (-2, 2) and the origin is sqrt(8).
         Since sqrt(8) < sqrt(10), (-2, 2) is closer to the origin.
         We only want the closest k = 1 points from the origin, so the answer is just [[-2,2]].
+
     Example 2:
-
-    Input: points = [[3,3],[5,-1],[-2,4]], k = 2
-    Output: [[3,3],[-2,4]]
-    Explanation: The answer [[-2,4],[3,3]] would also be accepted.    
-
+        Input: points = [[3,3],[5,-1],[-2,4]], k = 2
+        Output: [[3,3],[-2,4]]
+        Explanation: The answer [[-2,4],[3,3]] would also be accepted.    
 """
+
+# Time: O(k log n)
+# Space: O(n)
+# Where n is the length of the array points
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        minHeap = []
+        for x, y in points:
+            dist = (x ** 2) + (y ** 2)
+            minHeap.append([dist, x, y])
+
+        heapq.heapify(minHeap)
+        res = []
+        while k > 0:
+            dist, x, y = heapq.heappop(minHeap)
+            res.append([x, y])
+            k -= 1
+
+        return res
