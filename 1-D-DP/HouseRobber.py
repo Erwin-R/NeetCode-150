6f03a0df1fc267382defaee1b2c40b77c380a495
Erwin-R <rerwin684@gmail.com>
2026-01-27 15:21:05 -0600

    added house Robber algo and solution

---
+++ 1-D-DP/HouseRobber.py
@@ -0,0 +1,46 @@
+"""
+Problem: 
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping 
+you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent
+houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without 
+alerting the police.
+
+Test Cases: 
+    Example 1:
+        Input: nums = [1,2,3,1]
+        Output: 4
+        Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+        Total amount you can rob = 1 + 3 = 4.
+
+    Example 2:
+        Input: nums = [2,7,9,3,1]
+        Output: 12
+        Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+        Total amount you can rob = 2 + 9 + 1 = 12.
+"""
+
+# Memoization Solution
+# Time: O(n)
+# Space: O(n)
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # choosing the combo for each index 
+        # sub problem is finding out combos you can do for each index
+
+        cache = {}
+
+        def dfs(i):
+            if i >= len(nums):
+                return 0
+
+            if i in cache:
+                return cache[i]
+
+            cache[i] = max(dfs(i + 1), nums[i] + dfs(i + 2))
+            
+            return cache[i]
+
+        return dfs(0)
