79797743639d65817e67425878207e53126399b0
Erwin-R <rerwin684@gmail.com>
2026-02-03 14:25:44 -0600

    Added space optimized solution to house robber

--- 1-D-DP/HouseRobber.py
+++ 1-D-DP/HouseRobber.py
@@ -44,3 +44,18 @@ class Solution:
             return cache[i]
 
         return dfs(0)
+    
+
+# Space Optimized Solution
+# Time: O(n)
+# Space: O(1)
+class Solution2:
+    def rob(self, nums: List[int]) -> int:
+        rob1, rob2 = 0, 0
+
+        for n in nums: 
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        
+        return rob2
